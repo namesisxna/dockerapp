c65d89e3ff2ae18d167476bd9a3c26197ed346d9
ccconsole <sumitdatta2016@gmail.com>
2017-05-29 12:40:00 +0530

    added post method

--- src/main/java/com/cts/gto/ai/apiaitest/FlightController.java
+++ src/main/java/com/cts/gto/ai/apiaitest/FlightController.java
@@ -1,21 +1,35 @@
 package com.cts.gto.ai.apiaitest;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class FlightController {
 	
+	private static Logger LOGGER = LoggerFactory.getLogger(FlightController.class);
+	
 	@RequestMapping(value = "/flight", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public String getFlight() {
 		System.out.println("invoked");
 		return "flight booked";
+	}
+	
+	@RequestMapping(value = "/bookFlight", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String bookFlight(@RequestParam(value="name", defaultValue="India") String country) {
+		System.out.println("invoked bookFlight method");
+		LOGGER.info("invoked bookFlight method");
+		
+		return "you want to book flight from "+country;
 	}
 
 }
