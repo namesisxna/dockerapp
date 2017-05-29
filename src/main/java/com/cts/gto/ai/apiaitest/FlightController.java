0f0d0e1e9fd24b74ec57448b430a43bb1bf139fc
ccconsole <sumitdatta2016@gmail.com>
2017-05-29 17:09:25 +0530

    updated

--- src/main/java/com/cts/gto/ai/apiaitest/FlightController.java
+++ src/main/java/com/cts/gto/ai/apiaitest/FlightController.java
@@ -5,11 +5,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cts.gto.ai.datatypes.FlightQuery;
 import com.cts.gto.ai.util.ObjectToJson;
 
 
@@ -25,13 +27,13 @@ public class FlightController {
 		return "flight booked";
 	}
 	
-	@RequestMapping(value = "/bookFlight", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/bookFlight", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public String bookFlight(@RequestParam(value="geo-city", defaultValue="India") String country) {
+	public String bookFlight(@RequestBody() FlightQuery query) {
 		System.out.println("invoked bookFlight method");
 		LOGGER.info("invoked bookFlight method");
-		LOGGER.info("input request geo-city:"+country);
-		String jsonString = ObjectToJson.getJsonString("you want to book flight from "+country);
+		LOGGER.info("input request geo-city:"+query.getDestination());
+		String jsonString = ObjectToJson.getJsonString("you want to book flight from "+query.getSource());
 		LOGGER.info(jsonString);
 		return jsonString;
 	}
---
+++ src/main/java/com/cts/gto/ai/datatypes/FlightQuery.java
@@ -0,0 +1,19 @@
+package com.cts.gto.ai.datatypes;
+
+public class FlightQuery {
+	private String source;
+	private String destination;
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+}
