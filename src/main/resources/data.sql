aa2241701b5d7fa09153f21f14be6dde8db802fe
ccconsole <sumitdatta2016@gmail.com>
2017-06-02 12:04:07 +0530

    updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO flight (id,source, destination, IATA_Code,flight_id,Travel_Date) VALUES
-  (flight_id_seq.nextVal,'India', 'China', 'AGR','AI001',TO_DATE('2017-07-05', 'YYYY-MM-DD')),
-  (flight_id_seq.nextVal,'China', 'India', 'AKA','AI002',TO_DATE('2017-07-05', 'YYYY-MM-DD')),
+  (flight_id_seq.nextVal,'India', 'China', 'AGR','AI001',TO_DATE('2017-07-11', 'YYYY-MM-DD')),
+  (flight_id_seq.nextVal,'China', 'India', 'AKA','AI002',TO_DATE('2017-07-11', 'YYYY-MM-DD')),
   (flight_id_seq.nextVal,'India', 'China', 'AGR','AI003',TO_DATE('2017-07-06', 'YYYY-MM-DD')),
   (flight_id_seq.nextVal,'China', 'India', 'AKA','AI004',TO_DATE('2017-07-07', 'YYYY-MM-DD')),
    (flight_id_seq.nextVal,'India', 'China', 'AGR','AI005',TO_DATE('2017-07-08', 'YYYY-MM-DD')),
